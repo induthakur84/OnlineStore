76431c5b8d74edf3a81acc7566c5e5eda170044a
Indu Bala <indubala@gmail.com>
2026-08-21 23:01:59 +0530

    Add project files.

---
+++ Services/Order/Order.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Order.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Services/Payment/Payment.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Payment.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
